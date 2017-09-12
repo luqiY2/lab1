dcd1323b726d19c074462ef6430fac88dfc97119
jerrylui803 <jerrylui803@gmail.com>
2017-09-12 11:44:17 -0400

    changes by jerry

--- lab1test.py
+++ lab1test.py
@@ -1,7 +1,8 @@
 print("1st Line");
 print("2nd Line");
 print("3rd Line");
-print("4th Line");
+print("3.5th line");
+print("4th Line Jerry!!");
 print("5th Line");
 print("6th Line");
 print("7th Line");
